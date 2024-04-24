6e20706135f732b831704e4d10c0ef04c738b0ac
lucian vanghele <lucian.vanghele@ing.com>
2024-04-24 09:17:11 +0200

    adding tls

--- cmd/ebpf_exporter/main.go
+++ cmd/ebpf_exporter/main.go
@@ -12,35 +12,45 @@ import (
 	"strings"
 	"time"
 
+	kingpin "github.com/alecthomas/kingpin/v2"
 	"github.com/aquasecurity/libbpfgo"
 	"github.com/cloudflare/ebpf_exporter/v2/config"
 	"github.com/cloudflare/ebpf_exporter/v2/exporter"
 	"github.com/cloudflare/ebpf_exporter/v2/tracing"
 	"github.com/coreos/go-systemd/activation"
+	"github.com/go-kit/log/level"
 	"github.com/mdlayher/sdnotify"
 	"github.com/prometheus/client_golang/prometheus"
 	version_collector "github.com/prometheus/client_golang/prometheus/collectors/version"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/promlog"
+	"github.com/prometheus/common/promlog/flag"
 	"github.com/prometheus/common/version"
-	"gopkg.in/alecthomas/kingpin.v2"
+	"github.com/prometheus/exporter-toolkit/web"
+	"github.com/prometheus/exporter-toolkit/web/kingpinflag"
 	"kernel.org/pub/linux/libs/security/libcap/cap"
 )
 
 func main() {
-	configDir := kingpin.Flag("config.dir", "Config dir path.").Required().ExistingDir()
-	configNames := kingpin.Flag("config.names", "Comma separated names of configs to load.").Required().String()
-	configCheck := kingpin.Flag("config.check", "Check whether configs attach and exit.").Bool()
-	configStrict := kingpin.Flag("config.strict", "Make sure every probe registered.").Bool()
-	debug := kingpin.Flag("debug", "Enable debug.").Bool()
-	noLogTime := kingpin.Flag("log.no-timestamps", "Disable timestamps in log.").Bool()
-	listenAddress := kingpin.Flag("web.listen-address", "The address to listen on for HTTP requests (fd://0 for systemd activation).").Default(":9435").String()
-	metricsPath := kingpin.Flag("web.telemetry-path", "Path under which to expose metrics.").Default("/metrics").String()
-	capabilities := kingpin.Flag("capabilities.keep", "Comma separated list of capabilities to keep (cap_syslog, cap_bpf, etc.), 'all' or 'none'").Default("all").String()
-	btfPath := kingpin.Flag("btf.path", "Optional BTF file path.").Default("").String()
+	var (
+		configDir     = kingpin.Flag("config.dir", "Config dir path.").Required().ExistingDir()
+		configNames   = kingpin.Flag("config.names", "Comma separated names of configs to load.").Required().String()
+		configCheck   = kingpin.Flag("config.check", "Check whether configs attach and exit.").Bool()
+		configStrict  = kingpin.Flag("config.strict", "Make sure every probe registered.").Bool()
+		debug         = kingpin.Flag("debug", "Enable debug.").Bool()
+		listenAddress = kingpin.Flag("fd.listen-address", "The address to listen on for HTTP requests (fd://0 for systemd activation).").Default(":9435").String()
+		noLogTime     = kingpin.Flag("log.no-timestamps", "Disable timestamps in log.").Bool()
+		metricsPath   = kingpin.Flag("web.telemetry-path", "Path under which to expose metrics.").Default("/metrics").String()
+		capabilities  = kingpin.Flag("capabilities.keep", "Comma separated list of capabilities to keep (cap_syslog, cap_bpf, etc.), 'all' or 'none'").Default("all").String()
+		btfPath       = kingpin.Flag("btf.path", "Optional BTF file path.").Default("").String()
+		toolkitFlags  = kingpinflag.AddFlags(kingpin.CommandLine, ":9435")
+	)
+	promlogConfig := &promlog.Config{}
 	kingpin.Version(version.Print("ebpf_exporter"))
 	kingpin.HelpFlag.Short('h')
+	flag.AddFlags(kingpin.CommandLine, promlogConfig)
 	kingpin.Parse()
-
+	logger := promlog.New(promlogConfig)
 	libbpfgoCallbacks := libbpfgo.Callbacks{Log: libbpfLogCallback}
 	if !*debug {
 		libbpfgoCallbacks.LogFilters = append(libbpfgoCallbacks.LogFilters, func(libLevel int, _ string) bool {
@@ -149,45 +159,50 @@ func main() {
 		log.Printf("Debug enabled, exporting raw maps on /maps")
 		http.HandleFunc("/maps", e.MapsHandler)
 	}
+	if strings.HasPrefix(*listenAddress, "fd://") {
+		listener, err := listen(*listenAddress)
+		if err != nil {
+			log.Fatalf("Error listening on %s: %v", *listenAddress, err)
+		}
 
-	listener, err := listen(*listenAddress)
-	if err != nil {
-		log.Fatalf("Error listening on %s: %v", *listenAddress, err)
-	}
+		notify("ready with %s", readyString)
 
-	notify("ready with %s", readyString)
+		if notifier != nil {
+			_ = notifier.Notify(sdnotify.Ready)
+		}
 
-	if notifier != nil {
-		_ = notifier.Notify(sdnotify.Ready)
-	}
-
-	err = http.Serve(listener, nil)
-	if err != nil {
-		log.Fatalf("Error serving HTTP: %v", err)
+		err = http.Serve(listener, nil)
+		if err != nil {
+			log.Fatalf("Error serving HTTP: %v", err)
+		}
+	} else {
+		server := &http.Server{}
+		if err := web.ListenAndServe(server, toolkitFlags, logger); err != nil {
+			level.Error(logger).Log("err", err)
+			os.Exit(1)
+		}
 	}
 }
 
 func listen(addr string) (net.Listener, error) {
 	log.Printf("Listening on %s", addr)
-	if strings.HasPrefix(addr, "fd://") {
-		fd, err := strconv.Atoi(strings.TrimPrefix(addr, "fd://"))
-		if err != nil {
-			return nil, fmt.Errorf("error extracting fd number from %q: %v", addr, err)
-		}
 
-		listeners, err := activation.Listeners()
-		if err != nil {
-			return nil, fmt.Errorf("error getting activation listeners: %v", err)
-		}
-
-		if len(listeners) < fd+1 {
-			return nil, errors.New("no listeners passed via activation")
-		}
-
-		return listeners[fd], nil
+	fd, err := strconv.Atoi(strings.TrimPrefix(addr, "fd://"))
+	if err != nil {
+		return nil, fmt.Errorf("error extracting fd number from %q: %v", addr, err)
 	}
 
-	return net.Listen("tcp", addr)
+	listeners, err := activation.Listeners()
+	if err != nil {
+		return nil, fmt.Errorf("error getting activation listeners: %v", err)
+	}
+
+	if len(listeners) < fd+1 {
+		return nil, errors.New("no listeners passed via activation")
+	}
+
+	return listeners[fd], nil
+
 }
 
 func ensureCapabilities(keep string) error {
--- go.mod
+++ go.mod
@@ -3,37 +3,43 @@ module github.com/cloudflare/ebpf_exporter/v2
 go 1.21
 
 require (
+	github.com/alecthomas/kingpin/v2 v2.4.0
 	github.com/aquasecurity/libbpfgo v0.6.0-libbpf-1.3
 	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf
 	github.com/elastic/go-perf v0.0.0-20191212140718-9c656876f595
+	github.com/go-kit/log v0.2.1
 	github.com/iovisor/gobpf v0.2.0
 	github.com/jaypipes/pcidb v1.0.0
 	github.com/mdlayher/sdnotify v1.0.0
 	github.com/prometheus/client_golang v1.19.0
 	github.com/prometheus/common v0.51.1
+	github.com/prometheus/exporter-toolkit v0.11.0
 	go.opentelemetry.io/contrib/exporters/autoexport v0.49.0
 	go.opentelemetry.io/otel v1.24.0
 	go.opentelemetry.io/otel/sdk v1.24.0
 	go.opentelemetry.io/otel/trace v1.24.0
 	golang.org/x/sys v0.18.0
-	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 	gopkg.in/yaml.v2 v2.4.0
 	kernel.org/pub/linux/libs/security/libcap/cap v1.2.69
 )
 
 require (
-	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20231202071711-9a357b53e9c9 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.1 // indirect
+	github.com/jpillora/backoff v1.0.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
 	github.com/prometheus/client_model v0.6.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v1.24.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp v1.24.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.24.0 // indirect
@@ -45,8 +51,12 @@ require (
 	go.opentelemetry.io/otel/metric v1.24.0 // indirect
 	go.opentelemetry.io/otel/sdk/metric v1.24.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.1.0 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/net v0.23.0 // indirect
+	golang.org/x/oauth2 v0.18.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240221002015-b0ce06bbee7c // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240221002015-b0ce06bbee7c // indirect
 	google.golang.org/grpc v1.62.0 // indirect
